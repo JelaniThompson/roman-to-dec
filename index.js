e3adbc4f36bbe62fc238dad0a63f4090af1b9044
Jamen Marzonie <jamenmarz@gmail.com>
2016-04-25 20:09:53 -0600

    Fix some terms

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ exports = module.exports = function rtd(roman) {
   }
 
   // Convert each numeral to a base-10 numer:
-  var numerals = roman.split('').map(function map(numeral) {
-    return exports.romanMap[numeral] || 0;
+  var rep = roman.split('').map(function map(numeral) {
+    return exports.numerals[numeral] || 0;
   });
 
   // Use a numeral decoding scheme:
-  return numerals.reduce(function decode(prev, cur, i, n) {
+  return rep.reduce(function decode(prev, cur, i, n) {
     var next = n[i + 1] || 0;
     return cur < next ? prev - cur : prev + cur;
   }, 0);
 };
 
-exports.romanMap = {
+exports.numerals = {
   i: 1,
   v: 5,
   x: 10,
@@ -29,8 +29,3 @@ exports.romanMap = {
   d: 500,
   m: 1000
 };
-
-['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
-.forEach(function(cur) {
-  console.log(cur, exports(cur));
-});
